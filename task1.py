1b238c57371a2ab35b2edce1ea2746cfd1163fcb
baniinjeti <samuelinjeti4@gmail.com>
2022-12-27 22:18:31 +0530

    first file push to the git

---
+++ task1.py
@@ -0,0 +1,57 @@
+#   function dictinary task:
+  
+  
+dict1 ={
+"one":1,
+"two":2,
+"three":{
+    "four":4,
+    "five":5,
+    "six":{
+        "seven":7,
+        "eight":8    
+        },
+        "nine":9
+},
+"zero":0
+
+
+}   
+
+# for changin into captial letters onlyu outer
+# def conv_dict(dict1):
+#     new_dict={}
+#     for ele in dict1:
+#         print(ele)
+#         new_dict[ele.upper()]=dict1[ele]
+#     return (new_dict)
+# print(conv_dict(dict1))
+
+# def conv_dict(dict1):
+#     new_dict={}
+#     for ele in dict1:
+#         print(ele)
+#         new_dict[ele.upper()]=dict1[ele]
+#     return(new_dict)
+
+# print( conv_dict(dict1))
+
+
+ 
+# for changin cap in inner also
+
+
+
+# def conv_dict(dict1):
+#     new_dict={}
+#     for ele in dict1:
+#         print(ele)
+#         if type(dict1[ele]) ==dict:
+#             new_dict[ele.upper()]=conv_dict(dict1[ele])
+#         else:
+#             new_dict[ele.upper()]=dict1[ele]
+#     return(new_dict)
+
+# print( conv_dict(dict1))
+
+
